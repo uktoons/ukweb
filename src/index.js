ab4364af6d00d7ae910549ebed3ee02500041c22
uktoons <ccuukaoha@gmail.com>
2024-05-27 03:54:21 -0700

    files

--- src/index.js
+++ src/index.js
@@ -1,58 +1,21 @@
-const express=require("express")
-const app=express()
-const path=require('path')
-const hbs=require("hbs")
-const collection=require("./mongodb")
+const express = require('express');
+const { connectDB } = require('./mongodb');
+require('dotenv').config();
 
-const publicPath = path.join(__dirname, '../public');
-app.use(express.static(publicPath));
-const templatePath = path.join(__dirname, '../templates');
-app.set('views', templatePath);
-app.set('view engine', 'hbs');
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-app.get("/",(req,res)=>{
-    res.render("login")
-})
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/login', (req, res) => {
-    res.render('login');
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
-app.get("/signup",(req,res)=>{
-    res.render("signup")
-})
 
-app.post("/signup",async (req,res)=>{
-const data={
-    name:req.body.name,
-    password:req.body.password,
-    email:req.body.email
-}
-await collection.insertMany([data])
-
-res.render("home")
-})
-
-app.post("/login",async (req,res)=>{
-    
-   try{
-    const check=await collection.findOne({name:req.body.name})
-
-    if(check.password===req.body.password){
-        res.render("home")
-    }
-  else{
-    res.send("wrong password")
-  }
-   }
-   catch (error) {
-   res.send("sorry this user credential doesn't exist")
-   }
-    })
-
-    const PORT = process.env.PORT || 3000;
-
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-    
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,30 +1,33 @@
-const mongoose=require("mongoose")
+const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://uktoonspro:Iamuktoons,081976@uktoons.9ru0ng2.mongodb.net/?retryWrites=true&w=majority&appName=uktoons/ukweb")
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true, // Deprecated in MongoDB driver version 4, but still required for compatibility
+      useUnifiedTopology: true, // Deprecated in MongoDB driver version 4, but still required for compatibility
+    });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB:", err);
+    process.exit(1);
+  }
+};
 
-.then(()=>{
-    console.log("uk data base connected");
-})
-.catch(()=>{
-    console.log("failed");
-})
+const LogInSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  }
+});
 
-const LogInSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required: true,
-       
-    }
-})
+const User = mongoose.model("User", LogInSchema);
 
-const collection=new mongoose.model("collection1",LogInSchema)
-
-module.exports=collection
+module.exports = { connectDB, User };
